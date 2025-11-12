b3b6a02e67bbd243ee0cd97bab3e0200c2b6dcf6
Joseph Ryan <93546701+josephthehighiqidiot@users.noreply.github.com>
2025-11-12 21:00:07 +0000

    e

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+let img;
+let timesClicked = 0;
+let counter;
 document.getElementById("button").onclick = function() {
+    if (document.body.contains(img))
+    {
+        document.body.removeChild(img);
+    }
+    else
+    {
+        counter = document.createElement('p');
+        document.body.appendChild(counter)
+    }
+    if (timesClicked == 67
+        
     alert("you did it!");
-    const img = document.createElement('img');
-    img.src = 'image.png';
+    img = document.createElement('img');
+    // pick a random integer 0..5 and use it in the filename
+    img.src = `image${Math.floor(Math.random() * 6)}.webp`;
     img.style.opacity = '0';
     document.body.appendChild(img);
     
     // Trigger fade-in animation
     setTimeout(() => {
-        img.style.transition = 'opacity 5s ease-in';
+        img.style.transition = 'opacity 1s ease-in';
         img.style.opacity = '1';
-    }, 10);
+    }, 2);
+    timesClicked += 1;
+    counter.textContent = `Button clicked: ${timesClicked} times`;
+    
+    
 };
